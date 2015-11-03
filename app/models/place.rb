a0624987786342b3fd7d94b07fc78e05b0b97d52
Dmitriy K <fobfreeman@gmail.com>
2015-11-03 23:52:17 +0000

    generate the places scaffold

---
+++ app/models/place.rb
@@ -0,0 +1,8 @@
+class Place < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :phone
+  validates_presence_of :address
+  validates_presence_of :website
+  validates_presence_of :user_id
+  belongs_to :user
+end
